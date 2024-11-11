37060a2a23302c506206885c1f977600542aeb49
Soojin Ro <sugarpoint33@gmail.com>
2024-11-11 18:49:43 -0500

    Chapter2 - Data

---
+++ Chapter2/01-rational-numbers.rkt
@@ -0,0 +1,56 @@
+#lang sicp
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+; Exercise 2.1
+(define (make-rat n d)
+  (let ((g (gcd n d))
+        (sign (if (or (and (> n 0) (> d 0))
+                      (and (< n 0) (< d 0)))
+                  1
+                  -1)))
+    (cons (/ (* sign (abs n)) g) (/ (abs d) g))))
+
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+(print-rat one-third)
+
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (add-rat one-third one-third))
+
+
+(print-rat (make-rat 1 -3))
+(print-rat (make-rat -1 -3))
+(print-rat (make-rat -1 3))
