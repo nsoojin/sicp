f49c861ed05e8e1048ed8bbb74b29b05ca17cce8
Soojin Ro <sugarpoint33@gmail.com>
2024-12-06 16:57:11 -0500

    symbolic data

--- Chapter2/execise2-37.rkt
+++ Chapter2/execise2-37.rkt
@@ -12,6 +12,3 @@
 
 (define (dot-product v w)
   (accumulate + 0 (map * v w)))
-
-(define (matrix-*-vector m v)
-  
---
+++ Chapter2/symbolic-data.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define a 1)
+(define b 2)
+
+(list a b)
+
+(list 'a 'b)
+
+(list 'a b)
+
+(list 'a 'b 'c)
+
+(list (list 'george))
+
+(cdr '((x1 x2) (y1 y2)))
+
