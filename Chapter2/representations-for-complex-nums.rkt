6c2a305ee7945b2c7e7ce9ef41b0fcd891964f3f
Soojin Ro <sugarpoint33@gmail.com>
2024-12-08 16:53:30 -0500

    Rep for complex numbers

---
+++ Chapter2/representations-for-complex-nums.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define (attach-tag type-tag contents)
+  (cons type-tag contents))
+
+(define (type-tag datum)
+  (if (pair? datum)
+      (car datum)
+      (error "Bad tagged datum: TYPE-TAG" datum)))
+
+(define (contents datum)
+  (if (pair? datum)
+      (cdr datum)
+      (error "Bad tagged datum: CONTENTS" datum)))
+
+(define (rectangular? z)
+  (eq? (type-tag z) 'rectangular))
+
+(define (polar? z)
+  (eq? (type-tag z) 'polar))
+
